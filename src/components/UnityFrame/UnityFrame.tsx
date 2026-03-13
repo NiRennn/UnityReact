85a14be7e01e4954e91b9040ef4091b9e938b165
NiRenn <kigor768@gmail.com>
2026-03-13 13:26:03 +0300

    loader

--- src/components/UnityFrame/UnityFrame.tsx
+++ src/components/UnityFrame/UnityFrame.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useRef } from "react";
 
 export default function UnityFrame() {
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    const handleMessage = (event:MessageEvent) => {
+      if (event.source !== iframeRef.current?.contentWindow) return;
+
+      const data = event.data;
+
+      if (data?.source !== "unity") return;
+
+      switch (data.type) {
+        case "FOOD_PICKED":
+          console.log("[Unity -> React] Еда съедена:", data.payload?.score);
+          break;
+
+        case "GAME_OVER":
+          console.log("[Unity -> React] Игра окончена. Финальный счёт:", data.payload?.score);
+          break;
+
+        default:
+          console.log("[Unity -> React] Неизвестное событие:", data);
+          break;
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
+
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <iframe
+        ref={iframeRef}
         src="/unity/index.html"
         title="Unity Game"
         style={{ width: "100%", height: "100%", border: 0 }}
